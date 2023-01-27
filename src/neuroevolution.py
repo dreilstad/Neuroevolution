595da2eb02ba5bb9fdf6e075c8d3ddd7a4eb0305
Didrik Spanne Reilstad <didrikre@ml3.hpc.uio.no>
2023-01-27 09:38:46 +0100

    added results from initial connection test

--- src/neuroevolution.py
+++ src/neuroevolution.py
@@ -48,7 +48,7 @@ class Neuroevolution:
         """
 
         if self.evaluator is not None:
-            evaluator = self.evaluator(mp.cpu_count()//6, self.simulator)
+            evaluator = self.evaluator(mp.cpu_count()//4, self.simulator)
             best_genome = self.pop.run(evaluator.evaluate_genomes, n=self.num_generations)
         else:
             best_genome = self.pop.run(self.simulator.evaluate_genomes, n=self.num_generations)
