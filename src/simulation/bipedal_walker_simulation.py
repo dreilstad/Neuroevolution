fixed error in retina neural network setup, changed maze robot setup, fixed hamming distance calculation by normalizing inputs and outputs to between 0 and 1

src/simulation/bipedal_walker_simulation.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import gymnasium as gym
 from simulation.simulator import Simulator
@@ -11,6 +12,16 @@ class BipedalWalkerSimulator(Simulator):
         self.use_input_nodes_in_mod_div = True
         # 24 input nodes
         # 4 output nodes
+
+        low = np.array([-math.pi, -5.0, -5.0, -5.0, -math.pi, -5.0, -math.pi,
+                        -5.0, -0.0, -math.pi, -5.0, -math.pi, -5.0, -0.0] + [-1.0] * 10).astype(np.float32)
+        high = np.array([math.pi, 5.0, 5.0, 5.0, math.pi, 5.0, math.pi,
+                         5.0, 5.0, math.pi, 5.0, math.pi, 5.0, 5.0] + [1.0] * 10).astype(np.float32)
+        self.input_value_range = list(zip(low, high))
+
+        low = np.array([-1.0] * 4).astype(np.float32)
+        high = np.array([1.0] * 4).astype(np.float32)
+        self.output_value_range = list(zip(low, high))
 
     def simulate(self, neural_network):
 
@@ -41,7 +52,10 @@ class BipedalWalkerSimulator(Simulator):
 
             # save sequence if using hamming distance
             if self.hamming is not None:
-                sequence.extend([*state, *action])
+                # normalize input and output values to the range [0, 1]
+                norm_state = self._normalize_sequence(state, self.input_value_range)
+                norm_action = self._normalize_sequence(action, self.output_value_range)
+                sequence.extend([*norm_state, *norm_action])
 
             # append activations if using CKA or CCA
             if self.CKA is not None or self.CCA is not None:

src/simulation/tartarus_simulation.py
@@ -21,6 +21,10 @@ class TartarusSimulator(Simulator):
         # 11 input nodes (NW, N, NE, W, E, SW, S, SE, left, right, forward)
         # 3 output nodes (left, right, forward)
 
+        low = np.array([-1.0] * 8 + [0.0] * 3).astype(np.float32)
+        high = np.array([1.1] * 8 + [1.0] * 3).astype(np.float32)
+        self.input_value_range = list(zip(low, high))
+
     def simulate(self, neural_network):
 
         all_activations = None
@@ -48,7 +52,9 @@ class TartarusSimulator(Simulator):
 
                 # save sequence if using hamming distance
                 if self.hamming is not None:
-                    sequence.extend([*state, *output])
+                    # normalize input values to the range [0, 1]
+                    norm_state = self._normalize_sequence(state, self.input_value_range)
+                    sequence.extend([*norm_state, *output])
 
                 # append activations if using CKA or CCA
                 if self.CKA is not None or self.CCA is not None:

src/test.py
@@ -1,7 +1,39 @@
+import math
 import numpy as np
 import time
-from neat.nn.feed_forward import FeedForwardNetwork, FFNN
+from neat.nn.feed_forward import FeedForwardNetwork
 from util import load_checkpoints
+
+old_min = -1.0
+old_max = 1.0
+
+new_min = 0.0
+new_max = 1.0
+
+old_value = 0.0
+
+old_range = (old_max - old_min)
+new_range = (new_max - new_min)
+
+new_value = (((old_value - old_min) * new_range) / old_range) + new_min
+print(old_value)
+print(new_value)
+new_value = ((old_value - old_min) / (old_max - old_min)) * (new_max - new_min) + new_min
+print(new_value)
+
+low = np.array([-math.pi, -5.0, -5.0, -5.0, -math.pi, -5.0, -math.pi,
+                -5.0, -0.0, -math.pi, -5.0, -math.pi, -5.0, -0.0] + [-1.0] * 10).astype(np.float32)
+high = np.array([math.pi, 5.0, 5.0, 5.0, math.pi, 5.0, math.pi,
+                 5.0, 5.0, math.pi, 5.0, math.pi, 5.0, 5.0] + [1.0] * 10).astype(np.float32)
+
+print(low)
+print(high)
+print(zip(low, high))
+min_max_range = zip(low, high)
+for i, (old_min, old_max) in enumerate(min_max_range):
+    print(f"{i} = [{old_min} - {old_max}]")
+
+"""
 
 checkpoint_path = "/Users/didrik/Documents/Master/Neuroevolution/src/checkpoints/mazerobot-medium/performance-rep_div_cka/000"
 pop = load_checkpoints(checkpoint_path)[0]
@@ -31,7 +63,6 @@ print(f"Runtime (new version): {end - start} s")
 
 
 
-"""
 
 from simulation.environments.retina.retina_environment import HardRetinaEnvironment, VisualObject, Side
 